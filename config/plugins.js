3e4977e182032aac17f6dc05ce49ee0483755cbd
JoshBone <jozsef.bone@gmail.com>
2024-05-14 14:24:35 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,99 @@
 module.exports = {
-    // ...
-    'drag-drop-content-types': {
-      enabled: true
+  meilisearch: {
+    config: {
+      "annual-report"	: {
+        indexName: "website",
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            type: 'annualReport'
+          }
+        },
+      },
+      "collection": {
+        indexName: "website",
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            type: 'collection'
+          }
+        }
+      },
+      "entry": {
+        indexName: "website",
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            type: 'entry'
+          }
+        }
+      },
+      "event": {
+        indexName: "website",
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            type: 'event'
+          }
+        }
+      },
+      "fellow": {
+        indexName: "website",
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            type: 'fellow'
+          }
+        }
+      },
+      "job": {
+        indexName: "website",
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            type: 'job'
+          }
+        }
+      }, 
+      "news-item": {
+        indexName: "website",
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            type: 'news'
+          }
+        }
+      },
+      "project": {
+        indexName: "website",
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            type: 'project'
+          }
+        }
+      },
+      "staff-record": {
+        indexName: "website",
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            type: 'staff'
+          }
+        }
+      },
+      "static-page": {
+        indexName: "website",
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            type: 'page'
+          }
+        }
+      }
     }
+  },
+  'drag-drop-content-types': {
+    enabled: true
   }
+}
